4450800e84db17c25c5159787c7e25434e3b1808
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-10-03 17:23:30 +0100

    cleared code

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,21 +3,20 @@ import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
-    // Load environment variables
     const user = env('MONGODB_USER');
-    const password = encodeURIComponent(env('MONGODB_PASSWORD'));  // Encode special characters in the password
+    const password = encodeURIComponent(env('MONGODB_PASSWORD'));  
+    
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
-    // Log the MONGODB_USER environment variable
+  
     console.log('MONGODB_USER:', user);
 
-    // Construct MongoDB connection string
+  
     const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
 
-    console.log(`Connecting to MongoDB: ${DB_HOST.replace(password, '***')}`);  // Log the connection string without exposing the password
-
-    // Connect to MongoDB
+    console.log(`Connecting to MongoDB: ${DB_HOST.replace(password, '***')}`);  
+  
     await mongoose.connect(DB_HOST);
 
     console.log('Mongo connection successfully established!');
--- src/server.js
+++ src/server.js
@@ -36,14 +36,14 @@ export const setupServer = () => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
-    // Відповідь, якщо контакт не знайдено
+    
     if (!contact) {
       res.status(404).json({
         message: 'Contact not found',
       });
       return;
     }
-    // Відповідь, якщо контакт знайдено
+   
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
